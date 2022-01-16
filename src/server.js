ea02bee6e1efc554b0022c08db63dc264451951f
Avinash Malhotra <avimalhotra505@gmail.com>
2022-01-16 14:09:50 +0530

    Add data in db

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express=require("express");
 const path=require("path");
 const nunjucks=require("nunjucks");
 const app=express();
+const mongoose=require('mongoose');
 const db=require("./dao");
 const Car=require('./models/car');
 
@@ -16,6 +17,7 @@ nunjucks.configure(path.resolve(__dirname,'public'),{
     watch:true
 }); 
 
+
 app.get("/",(req,res)=>{
      Car.find({},(err,data)=>{
           if(err){
@@ -27,9 +29,45 @@ app.get("/",(req,res)=>{
       });
     
 });
+app.get("/add",(req,res)=>{
+     res.render('add.html',{title:"Add car"});
+ });
+
+ app.get("/addcar",(req,res)=>{
+     let carname=new Car({_id:new mongoose.Types.ObjectId(), name:req.query.name, power:req.query.power, torque:req.query.torque})
+
+      carname.save( (err, data)=> {
+          if (err){
+              console.log(err); 
+             }
+          else{
+              console.log(data.name + " saved to collection."); 
+             }
+        });
+      
+     res.render('add.html',{title:"Car Added"});
+ });
+
+ app.get("/car/:id",(req,res)=>{
+     let id=req.params.id;
+     //res.status(200).send('viewcar.html');
+     //res.render('viewcar.html',{title:id.name});
+     Car.find({name:id},(err,data)=>{
+          if(err){
+               res.render('home.html',{title:"Hello Express", error:err});
+          }
+          else{ 
+               res.render('viewcar.html',{title:data.name, data:data});
+          }
+      });
+    
+ });
 app.get("/contact",(req,res)=>{
      res.render('contact.html',{title:"Contact Page"});
  });
+
+
+ 
 
 app.get("/**",(req,res)=>{
      res.status(404).render('error.html',{title:"404 Error"});
